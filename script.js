cdff85e3a0fbed6af37d840f045d97d74c77d08b
obsskur <makolise77@gmail.com>
2025-05-25 17:47:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,13 @@ const commandInput = document.getElementById("command");
 const output = document.getElementById("output");
 
 commandInput.addEventListener("keydown", function (event) {
-  // Submit when pressing Ctrl+Enter
-  if (event.key === "Enter" && event.ctrlKey) {
+  if (event.key === "Enter") {
     event.preventDefault();
-    const command = commandInput.value.trim();
+    const command = commandInput.value;
     if (command !== "") {
       output.innerHTML += `$ ${command}\n`;
     }
     commandInput.value = "";
-    autoResize();
     window.scrollTo(0, document.body.scrollHeight);
-  } else {
-    autoResize();
   }
 });
-
-function autoResize() {
-  commandInput.style.height = "auto";
-  commandInput.style.height = commandInput.scrollHeight + "px";
-}
